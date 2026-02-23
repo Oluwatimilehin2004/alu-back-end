4cc7929cc53f363281352185584f8d85f3182698
Oluwatimilehin2004 <oluwatimilehinayo2004@gmail.com>
2026-02-23 09:01:58 -0800

    Omo i deh die

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Fetch and display TODO list progress for a given employee ID.
+Write a Python script that, using this REST API, for a given
+employee ID, returns information about his/her TODO list progress.
 """
 
 import requests
@@ -8,38 +9,41 @@ import sys
 
 
 if __name__ == "__main__":
-    user_id = sys.argv[1]
+    EMPLOYEE_ID = sys.argv[1]
 
-    user_url = (
+    USER_URL = (
         "https://jsonplaceholder.typicode.com/users/{}"
-        .format(user_id)
+        .format(EMPLOYEE_ID)
     )
-    todo_url = (
+    TODO_URL = (
         "https://jsonplaceholder.typicode.com/todos"
-        "?userId={}".format(user_id)
+        "?userId={}".format(EMPLOYEE_ID)
     )
 
-    user_response = requests.get(user_url)
-    todo_response = requests.get(todo_url)
+    USER_RESPONSE = requests.get(USER_URL)
+    TODO_RESPONSE = requests.get(TODO_URL)
 
-    user_data = user_response.json()
-    todos = todo_response.json()
+    USER_DATA = USER_RESPONSE.json()
+    TODOS = TODO_RESPONSE.json()
 
-    employee_name = user_data.get("name")
-    total_tasks = len(todos)
+    EMPLOYEE_NAME = USER_DATA.get("name")
+    TOTAL_NUMBER_OF_TASKS = len(TODOS)
 
-    done_tasks = [
-        task for task in todos
-        if task.get("completed")
-    ]
+    NUMBER_OF_DONE_TASKS = 0
+    for TASK in TODOS:
+        if TASK.get("completed"):
+            NUMBER_OF_DONE_TASKS += 1
 
     print(
         "Employee {} is done with tasks({}/{}):"
-        .format(employee_name,
-                len(done_tasks),
-                total_tasks)
+        .format(
+            EMPLOYEE_NAME,
+            NUMBER_OF_DONE_TASKS,
+            TOTAL_NUMBER_OF_TASKS
+        )
     )
 
-    for task in done_tasks:
-        print("\t {}".format(task.get("title")))
-        
+    for TASK in TODOS:
+        if TASK.get("completed"):
+            TASK_TITLE = TASK.get("title")
+            print("\t {}".format(TASK_TITLE))
